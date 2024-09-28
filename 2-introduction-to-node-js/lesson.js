8f52566b59216542367b352cbf0655200c38edd0
Hanna Negash <hnegash.n@gmail.com>
2024-09-27 18:45:01 -0700

    Add to make the heading in middle

--- 2-introduction-to-node-js/lesson.js
+++ 2-introduction-to-node-js/lesson.js
@@ -1,6 +1,8 @@
-// Introduction
+//------------------------------------------------------------ Introduction
 
 // Node.js is a JavaScript runtime, or an environment that allows us to execute JavaScript code outside of the browser. A “runtime”
 // converts code written in a high-level, human-readable, programming language and compiles it down to code the computer can execute.
 
 // (Node.js is already installed) so type node -v in the terminal to check the version of Node
+
+//------------------------------------------------------------ The Node REPL
