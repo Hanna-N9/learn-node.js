e1bd3ef4f571799360a929be5741ab8219a616bc
Hanna Negash <hnegash.n@gmail.com>
2024-09-27 20:47:17 -0700

    Incomplete codes on REPL

--- 2-introduction-to-node-js/lesson.js
+++ 2-introduction-to-node-js/lesson.js
@@ -3,6 +3,19 @@
 // Node.js is a JavaScript runtime, or an environment that allows us to execute JavaScript code outside of the browser. A “runtime”
 // converts code written in a high-level, human-readable, programming language and compiles it down to code the computer can execute.
 
-// (Node.js is already installed) so type node -v in the terminal to check the version of Node
+//---- (Node.js is already installed) so type node -v in the terminal to check the version of Node
 
 //------------------------------------------------------------ The Node REPL
+
+/* REPL is an abbreviation for read–eval–print loop. It’s a program that loops, or repeatedly cycles, through three different states: 
+a read state where the program reads input from a user, the eval state where the program evaluates the user’s input, and 
+the print state where the program prints out its evaluation to a console. Then it loops through these states again. */
+
+// Installing Node comes with a built-in JavaScript REPL. To access the REPL is to type the command node into the terminal and hit enter.
+//A > character will show up in the terminal, indicating the REPL is running and prompting your input. The Node REPL will evaluate your
+// input line by line.
+// To type multiple lines and then have them evaluated at once is to type .editor while in the REPL
+
+//--- To do to try out the Node REPL and explore the Node global object
+
+// Type node in the terminal and press enter to enter the Node REPL
