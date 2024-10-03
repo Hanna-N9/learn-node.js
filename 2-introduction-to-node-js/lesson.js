6b4a2bf60f14ddcb9ed8a1e4d4a8cf326ea3757e
Hanna Negash <hnegash.n@gmail.com>
2024-10-02 19:34:25 -0700

    Convert the single-line comment to block comment for multiple lines

--- 2-introduction-to-node-js/lesson.js
+++ 2-introduction-to-node-js/lesson.js
@@ -1,7 +1,7 @@
 //------------------------------------------------------------ Introduction
 
-// Node.js is a JavaScript runtime, or an environment that allows us to execute JavaScript code outside of the browser. A “runtime”
-// converts code written in a high-level, human-readable, programming language and compiles it down to code the computer can execute.
+/* Node.js is a JavaScript runtime, or an environment that allows us to execute JavaScript code outside of the browser. A “runtime”
+converts code written in a high-level, human-readable, programming language and compiles it down to code the computer can execute. */
 
 //---- (Node.js is already installed) so type node -v in the terminal to check the version of Node
 
@@ -12,20 +12,20 @@ a read state where the program reads input from a user, the eval state where the
 the print state where the program prints out its evaluation to a console. Then it loops through these states again. */
 
 // Installing Node comes with a built-in JavaScript REPL. To access the REPL is to type the command node into the terminal and hit enter.
-//A > character will show up in the terminal, indicating the REPL is running and prompting your input. The Node REPL will evaluate your
+// A > character will show up in the terminal, indicating the REPL is running and prompting your input. The Node REPL will evaluate your
 // input line by line.
 // To type multiple lines and then have them evaluated at once is to type .editor while in the REPL
 
-// REPLs are powerful tools for calculations, learning, and development, allowing exploration of language features with immediate feedback.
-// The Node environment provides a global object containing Node-specific properties and methods, enhancing functionality beyond standard
-// JavaScript capabilities.
+/* REPLs are powerful tools for calculations, learning, and development, allowing exploration of language features with immediate feedback.
+The Node environment provides a global object containing Node-specific properties and methods, enhancing functionality beyond standard
+JavaScript capabilities. */
 
 //---- To do to try out the Node REPL and explore the Node global object
 
 //---- Type node in the terminal and press enter to enter the Node REPL
 //---- To access global is to type global which bring huge contents because of global.process object
 //---- So to check out an easier-to-read list of the properties on the global object is to type Object.keys(global)
-// The global object contains numerous valuable properties and methods that are rarely modified. However, since the global object is
-// essentially a regular JavaScript object, we have the flexibility to customize it further if needed. For instance, we could add
-//--- a new property like global.cat = 'meow!' to extend its functionality then print or return the property by typing
-// console.log(global.cat)
+/* The global object contains numerous valuable properties and methods that are rarely modified. However, since the global object is
+essentially a regular JavaScript object, we have the flexibility to customize it further if needed. For instance, we could 
+--- Add a new property like global.cat = 'meow!' to extend its functionality then print or return the property by typing 
+console.log(global.cat) */
