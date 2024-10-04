7a9635c52966f9649c1713b69744d6eea7e8febe
Hanna Negash <hnegash.n@gmail.com>
2024-10-03 18:07:19 -0700

    More adjustment

--- 2-introduction-to-node-js/lesson.js
+++ 2-introduction-to-node-js/lesson.js
@@ -9,19 +9,17 @@ converts code written in a high-level, human-readable, programming language and 
 
 /* REPL is an abbreviation for read–eval–print loop. It’s a program that loops, or repeatedly cycles, through three different states: 
 a read state where the program reads input from a user, the eval state where the program evaluates the user’s input, and 
-the print state where the program prints out its evaluation to a console. Then it loops through these states again. */
+the print state where the program prints out its evaluation to a console. Then it loops through these states again. 
 
-/* 
 - Installing Node comes with a built-in JavaScript REPL. To access the REPL is to type the command node into the terminal and hit enter.
 A '>' character will show up in the terminal, indicating the REPL is running and prompting your input. The Node REPL will evaluate your input line by line.
-- To type multiple lines and then have them evaluated at once is to type .editor while in the REPL
- */
+- To type multiple lines and then have them evaluated at once is to type .editor while in the REPL.
 
-/* REPLs are powerful tools for calculations, learning, and development, allowing exploration of language features with immediate feedback.
+REPLs are powerful tools for calculations, learning, and development, allowing exploration of language features with immediate feedback.
 The Node environment provides a global object containing Node-specific properties and methods, enhancing functionality beyond standard
 JavaScript capabilities. */
 
-//---- To do to try out the Node REPL and explore the Node global object
+//----// To do to try out the Node REPL and explore the Node global object
 
 //---- Type node in the terminal and press enter to enter the Node REPL
 //---- To access global is to type global which bring huge contents because of global.process object
@@ -35,14 +33,14 @@ essentially a regular JavaScript object, we have the flexibility to customize it
 /* Node was designed for server-side web development and has thoughtful functionality for that end. At its most simple, however, it 
 allows us to run JavaScript programs on our own computers instead of just in the browser’s console or embedded in HTML. */
 
-//---- Explore some of the functionality and properties specific to the Node environment
+//----// Explore some of the functionality and properties specific to the Node environment
 
 /* To execute the code is to navigate to to the directory that contains the <specific file>.js with codes that you wrote there and 
 type the command node <specific file with written codes>.js into the terminal. */
 
 console.log("Hello World");
 
-//---- There are a number of variables assigned the string.
+//----// There are a number of variables assigned the string.
 
 let noun1 = "cat";
 let adjective = "silly";
@@ -55,3 +53,5 @@ console.log(
 );
 
 //---- Type node lesson.js (this directory's file) to run the program to see the output
+
+//------------------------------------------------------------ Core Modules
